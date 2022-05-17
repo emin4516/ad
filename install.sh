717517b696b02471ffc8a6663aaf5a1c2aef058f
emin4516 <70646344+emin4516@users.noreply.github.com>
2022-05-17 12:03:53 +0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+apt update
+apt upgrade
+apt install python3
+apt install postgresql
+pip install -r requirements.txt
---
+++ vps.sh
@@ -0,0 +1,6 @@
+apt update
+apt upgrade
+apt install python3
+apt install postgresql
+apt-get -y install python3-pip
+pip install -r requirements.txt
